855b4399a065aaff77630bee27ac42302611000c
Olejechka <144726472+Olejechka@users.noreply.github.com>
2025-06-17 16:15:45 +0300

    Add files via upload

--- ktrail/engine/car.py
+++ ktrail/engine/car.py
@@ -1,37 +1,34 @@
-# engine/screens/car.py
-
 from PyQt5.QtCore import QRect
-from random import choice
-
+from random import randint
+from PyQt5.QtGui import QPixmap
 
 class Car:
+    TEXTURE_PATHS = [f"assets/textures/car/car{i}.png" for i in range(1, 15)]
+
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
         self.screen_height = screen_height
 
-        # Размеры машины
         self.width = 384
         self.height = 768
 
-        # Правый ряд (аналогично препятствиям)
-        self.x = 192  # Фиксированная позиция — правый ряд
-        self.y = screen_height + self.height  # Начинаем ниже экрана
+        self.x = randint(-50, 388)
+        self.y = screen_height + self.height
 
-        # Скорость движения вверх
         self.speed = 10
 
-        # Загрузка текстуры
-        from PyQt5.QtGui import QPixmap
-        self.texture = QPixmap("assets/textures/dev_car.png").scaled(self.width, self.height)
+        texture_path = Car.TEXTURE_PATHS[randint(0, len(Car.TEXTURE_PATHS) - 1)]
+        self.texture = QPixmap(texture_path).scaled(self.width, self.height)
+
+    def draw(self, painter):
+        if self.texture and not self.is_off_screen():
+            painter.drawPixmap(self.x, self.y, self.texture)
 
     def move(self):
-        """Движение вверх"""
         self.y -= self.speed
 
     def is_off_screen(self):
-        """Проверяет, ушла ли машина за верх экрана"""
         return self.y + self.height < 0
 
     def get_rect(self):
-        """Возвращает QRect для проверки столкновений (если понадобится)"""
         return QRect(self.x, self.y, self.width, self.height)
